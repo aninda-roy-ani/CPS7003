f684d0a930ca5fc1cb149db41ae8d33449a1170e
Aninda Roy Ani <anindaroyy@gmail.com>
2024-02-14 14:47:14 +0000

    class1 codes

--- week3/hr.sql
+++ week3/hr.sql
@@ -8,3 +8,30 @@ CREATE TABLE employee (
     job_title TEXT,
     salary REAL
 );
+CREATE TABLE job_title (
+    job_title_id INTEGER PRIMARY KEY,
+    title_name TEXT,
+    description TEXT,
+    base_salary REAL
+);
+CREATE TABLE location (
+    location_id INTEGER PRIMARY KEY,
+    city TEXT,
+    state TEXT,
+    country TEXT
+);
+CREATE TABLE task (
+    task_id INTEGER PRIMARY KEY,
+    task_name TEXT,
+    description TEXT
+);
+CREATE TABLE employee_task (
+    employee_id INTEGER
+    task_id INTEGER
+);
+CREATE TABLE department (
+    department_id INTEGER PRIMARY KEY,
+    department_name TEXT,
+    manager_id INTEGER,
+    location_id INTEGER
+);
